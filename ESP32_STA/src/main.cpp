720a0974d2d81fa49bb378a6630e4b45991036b2
Florian <kadjiflorian@gmail.com>
2022-04-02 17:40:02 +0100

    Example ESP_STA Mode

--- ESP32_STA/src/main.cpp
+++ ESP32_STA/src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+ *  This sketch just try to connect to the given network with credentials
+ */
+
+#include <WiFi.h>
+
+// Network credentials
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+    delay(5000);
+
 }
