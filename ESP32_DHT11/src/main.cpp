41b1afeb5da10076c77f80ae7d48f153ed2c8e4c
Florian <kadjiflorian@gmail.com>
2022-04-03 04:56:36 +0100

    testing ESP32_DHT11

--- ESP32_DHT11/src/main.cpp
+++ ESP32_DHT11/src/main.cpp
@@ -2,10 +2,70 @@
 
 #include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#define DHTTYPE DHT11   // DHT 11
 
+// DHT Sensor
+const int DHTPin = 27;
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Timers auxiliar variables
+long now = millis();
+long lastMeasure = 0;
+
+void setup(void)
+{ 
+  dht.begin();
+  
+  Serial.begin(115200);
+
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+
+    now = millis();
+  // Publishes new temperature and humidity every 30 seconds
+  if (now - lastMeasure > 2000) {
+    lastMeasure = now;
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+//    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) ) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    // Computes temperature values in Celsius
+    float hic = dht.computeHeatIndex(t, h, false);
+    static char temperatureTemp[7];
+    dtostrf(hic, 6, 2, temperatureTemp);
+    
+    // Uncomment to compute temperature values in Fahrenheit 
+    // float hif = dht.computeHeatIndex(f, h);
+    // static char temperatureTemp[7];
+    // dtostrf(hic, 6, 2, temperatureTemp);
+    
+    static char humidityTemp[7];
+    dtostrf(h, 6, 2, humidityTemp);
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.println(" ");
+    
+/*    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.println(" *C ");
+*/
+    // Serial.print(hif);
+    // Serial.println(" *F");
+  }
 }
